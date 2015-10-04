c885d35234d5574a46722cd8a5f06649c0358493
andrzej560 <andrzej560@wp.pl>
2015-10-04 20:12:14 +0200

    main commit

---
+++ ReadyEmguCVProject/ReadyEmguCVProject.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ReadyEmguCVProject
+{
+    partial class ReadyEmguCVProject
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReadyEmguCVProject));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(393, 429);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(101, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Szukaj twarzy";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(8, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(658, 422);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 430);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Ładuj obrazek";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1.1",
+            "1.2",
+            "1.3",
+            "1.4"});
+            this.listBox1.Location = new System.Drawing.Point(313, 432);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(74, 30);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(244, 435);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Szybkość";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 470);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(213, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Im wyższa szybkość tym niższa dokładność";
+            // 
+            // ReadyEmguCVProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 490);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "ReadyEmguCVProject";
+            this.Text = "Wykrywanie twarzy";
+            this.Load += new System.EventHandler(this.ReadyEmguCVProject_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ReadyEmguCVProject/ReadyEmguCVProject.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;                 
+using Emgu.CV.Structure;      
+using Emgu.Util;               
+using Emgu.CV.CvEnum;
+namespace ReadyEmguCVProject
+{
+    public partial class ReadyEmguCVProject : Form
+    {
+
+
+
+        public ReadyEmguCVProject()
+        {
+            InitializeComponent(); //inicjalizacja komponentów
+        }
+
+
+        private HaarCascade haar; //detektor obiektów
+        private Image image; 
+      
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                Image<Bgr, byte> ImageFrame = new Image<Bgr, byte>(new Bitmap(image)); //pobranie obrazka
+
+                String text;
+                text = listBox1.GetItemText(listBox1.SelectedItem); //pobranie wartości z listy rozwijanej (wartości 1.1; 1.2; 1.3; 1.4)
+                double parameter;
+                try
+                {
+                    parameter = double.Parse(text.Replace(".", ",")); 
+                }
+                catch (Exception)
+                {
+                    parameter = 1.1; //wartość domyślna - najbardziej dokładne wykrywanie, lecz najwolniejsze
+                }
+                
+                if (ImageFrame != null)   //sprawdzenie czy obrazek został załadowany
+                {
+                    // konwersja obrazka zgodna z ideą algorytmu Violi-Jonesa
+                    Image<Gray, byte> grayframe = ImageFrame.Convert<Gray, byte>();
+
+                    //wykrywanie twarzy
+                    var faces = grayframe.DetectHaarCascade(haar, parameter, 4,
+                                            HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                                            new Size(25, 25))[0];
+
+                    int TotalFaces = faces.Length;   // zmienna przechowująca ilość pobranych twarzy
+
+                    MessageBox.Show("Wykryto: " + TotalFaces.ToString() + " twarzy");
+                    //zaznaczenie każdej znalezionej twarzy zieloną ramką
+                    foreach (var face in faces)
+                    {
+                        ImageFrame.Draw(face.rect, new Bgr(Color.Green), 3);
+                    }
+
+
+                }
+                
+                pictureBox1.Image = ImageFrame.ToBitmap();   //wyswietlenie obrazka
+                this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; //wyskalowanie obrazka
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Najpierw załaduj obrazek");
+            }
+
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void ReadyEmguCVProject_Load(object sender, EventArgs e)
+        {
+            haar = new HaarCascade("haarcascade_frontalface_alt_tree.xml"); //załadowanie bazy twarzy
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Wybierz plik";
+                dlg.Filter = "jpg files (*.jpg)|*.jpg";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    image = new Bitmap(dlg.FileName); //załadowanie wybranego obrazka
+                    pictureBox1.Image = image;
+                    this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                }
+            }
+
+
+        }
+
+    }
+}
